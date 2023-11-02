7e65b6b89f3d01a8dca2d7dd5ba1c74406e5d002
Andrew <lonfiyt@gmail.com>
2023-11-02 14:29:06 +0300

    modified files

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,7 +4,12 @@ const Main = () => {
   return (
   <main>
     <div className={styles.content}>
-      
+      <section className={styles.language__controller}>
+        <div className={`${styles.language} ${styles.choosen}`}>RUS</div>
+        <div className={styles.language}>ENG</div>
+        <div className={styles.language}>POR</div>
+        <div className={styles.language}>FRA</div>
+      </section>
     </div>
   </main>
   )
