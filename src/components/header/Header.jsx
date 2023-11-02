3b49c273bf61508ebd6495f8a836d0ee66ae17f8
Andrew <lonfiyt@gmail.com>
2023-11-02 14:02:22 +0300

    ready header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,22 +5,29 @@ import logo from '../../img/logotype.png'
 const Header = () => {
   return (
     <header>
-      <div className={styles.wrapper}>
-      <img src={logo} alt="" className={styles.img} />
-        <nav className={styles.navigation}>
-           <p className={`${styles.title} ${styles.choosen} ${styles.first}`}>Стратегия</p>
-           <p className={styles.title}>Раскидки</p>
-           <p className={styles.title}>Схемы игр</p>
-           <p className={styles.title}>Комьюнити</p>
-           <p className={styles.title}>Информация</p>
-           <p className={styles.title}>Вход</p>
+       <div className={styles.header__wrapper}>
+         <img src={logo} alt="" className={styles.header__img} />
+         <nav className={styles.header__navigation}>
+           <p className={`${styles.navigation__title} ${styles.choosen}`}>Стратегия</p>
+           <p className={styles.navigation__title}>Раскидки</p>
+           <p className={styles.navigation__title}>Схемы игр</p>
+           <p className={styles.navigation__title}>Комьюнити</p>
+           <p className={styles.navigation__title}>Информация</p>
+           <p className={styles.navigation__title}>Вход</p>
         </nav>
+       </div>
+
+
        <div className={styles.pro__wrap}>
         <div className={styles.pro}>
           PRO
          </div>
-       </div>
-      </div>
+
+            <div className={styles.burger}>
+              <span></span>
+            </div>
+
+       </div> 
     </header>
   )
 }
